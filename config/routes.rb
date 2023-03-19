485e49282e0f3c4d90728d708c8bf4cb693082bc
chen <zx656084>
2023-03-19 13:29:39 +0800

    commit1

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,20 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources:candidates
 end
+
+
+
+
+
+# REST
+# GET /members => list all 列出所有會員資料
+# GET /members/2 觀看二號會員資料
+# GET /members/2/edit 編輯2號會員資料 
+
+
+# REST
+# GET /books => list all 列出所有書的資料
+# GET /books/2 觀看二號會員資料
+# GET /books/2/edit 編輯第二本書的資料 
+
+# rails routes 印出專案內所有路徑對照表
